2b1820f1685fe2d05bd9007439e03384f2e4a4d8
Rafael <72451109+Rosmenteniuc-Rafael@users.noreply.github.com>
2020-10-17 16:38:00 +0300

    Add files via upload

---
+++ tema1_moore/tema1_moore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace tema1_moore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            uint n;
+
+            Console.WriteLine(" Introduceti un numar natural n, pentru a afla in cat timp procesorul va deveni de n-ori mai puteric fata de momentul actual:");
+            while (true)
+            {
+                Console.Write("Introduceti un numar natural:");
+                input = Console.ReadLine();
+                if (uint.TryParse(input, out n))
+                {
+                    double ani;
+                    ani = 1.5 * Math.Log(n, 2);
+                    Console.WriteLine("Procesorul va fi mai rapid de " + n + " ori in " + ani + " ani");
+                    Console.WriteLine("Procesorul va fi mai rapid de {0} ori in {1} ani", n, ani);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Introduceti un numar natural!\n");
+                }
+            }
+        }
+    }
+}
